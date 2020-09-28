ff1c945f44a3ff9c04d2d94dd24329ffbf0579d7
Goshen Jimenez <goshen.jimenez@obelisksecurity.com>
2020-09-28 22:42:47 +0800

    Main Window code

--- GoshenJimenez.EmployeesDatabaseSystem/GoshenJimenez.EmployeesDatabaseSystem.Windows/MainWindow.xaml.cs
+++ GoshenJimenez.EmployeesDatabaseSystem/GoshenJimenez.EmployeesDatabaseSystem.Windows/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GoshenJimenez.EmployeesDatabaseSystem.Windows.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,15 @@ namespace GoshenJimenez.EmployeesDatabaseSystem.Windows
         public MainWindow()
         {
             InitializeComponent();
+
+            EmployeesDBContext context = new EmployeesDBContext();
+
+            var employee = context.Employees.FirstOrDefault();
+
+            if(employee != null)
+            {
+                MessageBox.Show(employee.EmailAddress);
+            }
         }
     }
 }
