92e2b4740aa2706cc636b80bc3c7a6676e96cb10
Goshen Jimenez <goshen.jimenez@obelisksecurity.com>
2020-09-28 22:32:20 +0800

    Data Initializer

---
+++ GoshenJimenez.EmployeesDatabaseSystem/GoshenJimenez.EmployeesDatabaseSystem.Windows/DAL/DataInitializer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoshenJimenez.EmployeesDatabaseSystem.Windows.DAL
+{
+    public class DataInitializer : System.Data.Entity.DropCreateDatabaseAlways<EmployeesDBContext>
+    {
+        protected override void Seed(EmployeesDBContext context)
+        {
+            context.Employees.Add(new Models.Employee()
+            {
+                Id = Guid.Parse("56bac4c3-ef14-4f17-905d-5fb554063561"),
+                FirstName = "Jace",
+                LastName = "Beleren",
+                Assignment = Models.Enums.Assignment.Office,
+                EmailAddress = "jbeleren@mailinator.com",
+                Salary = decimal.Parse("100000"),
+                Status = Models.Enums.Status.Regular
+            });
+
+            context.Employees.Add(new Models.Employee()
+            {
+                Id = Guid.Parse("56bac4c3-ef14-4f17-905d-5fb554063562"),
+                FirstName = "Liliana",
+                LastName = "Vess",
+                Assignment = Models.Enums.Assignment.Field,
+                EmailAddress = "lvess@mailinator.com",
+                Salary = decimal.Parse("120000"),
+                Status = Models.Enums.Status.Probationary
+            });
+
+            context.SaveChanges();
+        }
+    }
+}
--- GoshenJimenez.EmployeesDatabaseSystem/GoshenJimenez.EmployeesDatabaseSystem.Windows/DAL/EmployeesDBContext.cs
+++ GoshenJimenez.EmployeesDatabaseSystem/GoshenJimenez.EmployeesDatabaseSystem.Windows/DAL/EmployeesDBContext.cs
@@ -9,6 +9,11 @@ namespace GoshenJimenez.EmployeesDatabaseSystem.Windows.DAL
 {
     public class EmployeesDBContext : DbContext
     {
+        public EmployeesDBContext() : base("myConnectionString")
+        {
+            Database.SetInitializer(new GoshenJimenez.EmployeesDatabaseSystem.Windows.DAL.DataInitializer());
+        }
+
         public DbSet<Models.Employee> Employees { get; set; }
 
     }
