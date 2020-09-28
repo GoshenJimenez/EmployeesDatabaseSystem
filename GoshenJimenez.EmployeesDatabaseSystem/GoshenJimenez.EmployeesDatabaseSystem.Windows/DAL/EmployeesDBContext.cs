3193897ba22411582c82187852d9459ad48158e7
Goshen Jimenez <goshen.jimenez@obelisksecurity.com>
2020-09-28 22:19:51 +0800

    DbContext for in DAL folder

---
+++ GoshenJimenez.EmployeesDatabaseSystem/GoshenJimenez.EmployeesDatabaseSystem.Windows/DAL/EmployeesDBContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoshenJimenez.EmployeesDatabaseSystem.Windows.DAL
+{
+    public class EmployeesDBContext : DbContext
+    {
+        public DbSet<Models.Employee> Employees { get; set; }
+
+    }
+}
